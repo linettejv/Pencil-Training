be8a4b8667f7a4c7467cd2b616e23f9ec19384de
Mithun <mithunk094619@gmail.com>
2023-09-20 18:07:29 +0530

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Sidenav from "@/components/sidenav";
 import CopyScript from "@/components/copyScript";
@@ -11,12 +11,9 @@ import { useState } from "react";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-full">
+    <div className="flex min-h-screen flex-row  justify-between bg-gray-2">
+      <Sidenav />
       <CopyScript />
-      <div className="flex min-h-screen flex-row  justify-between bg-gray-2">
-        <Sidenav />
-        <div>landing page</div>
-      </div>
     </div>
   );
 }
