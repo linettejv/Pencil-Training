f5b702849e2b741a6321eb2865746ac70a0820ac
Mithun <mithunk094619@gmail.com>
2023-09-21 16:20:18 +0530

    component incomplete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
 "use client";
 
-import Sidenav from "@/components/sidenav";
-import CopyScript from "@/components/copyScript";
-import InputControlledComponent from "@/components/input";
-import InputLabelPair from "@/components/inputLabelPair";
-import Label from "@/components/label";
-import InputTextareaPair from "@/components/labelTextareaPair";
-
+import { Select } from "@/components/select";
 import { useState } from "react";
 
+const objectives = [
+  { id: "Conversions", label: "CONVERSIONS" },
+  { id: "Awareness", label: "AWARENESS" },
+  { id: "Traffic", label: "TRAFFIC" },
+];
+
 export default function Home() {
+  const [objective, setObjective] = useState(objectives[0]);
   return (
     <div className="flex min-h-screen flex-row  justify-between bg-gray-2">
-      <Sidenav />
-      <CopyScript />
+      {/* <Sidenav />
+      <CopyScript /> */}
+      <Select
+        label="Objectives"
+        options={objectives}
+        selectedOption={objective}
+        selectedOptionLabelKey="id"
+        onOptionClick={(option) => {setObjective(option)}}
+        customWidth="w-[190px]"
+        isUppercaseLabel={true}
+        isThemeSensitive={true}
+      />
     </div>
   );
 }
